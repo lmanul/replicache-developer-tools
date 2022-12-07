dce66d83525fb342af52e1711d768a343b2ece8f
Manu Cornet <m@ma.nu>
2022-12-07 21:07:29 +0000

    Move the content script installation from the panel to the background script, seems to make more sense.

--- background.js
+++ background.js
@@ -1,5 +1,35 @@
+const CONTENT_SCRIPT_ID = 'replicache_devtools_hook';
+
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete' && tab.active) {
     console.log('Tab updated');
   }
 })
+
+console.log('Add listener, this is toplevel');
+chrome.runtime.onConnect.addListener(port => {
+  console.log('Add listener on runtime');
+  port.onMessage.addListener(msg => {
+    console.log('Got message', msg);
+  });
+});
+
+const initialize = async () => {
+  const registered = await chrome.scripting.getRegisteredContentScripts({
+    ids: [CONTENT_SCRIPT_ID],
+  });
+  if (!!registered.length) {
+    console.log('Hook already installed');
+  } else {
+    console.log('Installing hook...');
+    chrome.scripting.registerContentScripts([{
+        id: CONTENT_SCRIPT_ID,
+        matches: ['<all_urls>'],
+        js: ['install_hook.js'],
+        runAt: 'document_start',
+        world: chrome.scripting.ExecutionWorld.MAIN,
+    }]);
+  }
+};
+
+initialize();
--- panel.js
+++ panel.js
@@ -1,25 +1,10 @@
 console.log('panel JS');
 
-const CONTENT_SCRIPT_ID = 'replicache_devtools_hook';
+let port;
 
 const initialize = async () => {
   console.log('Initializing...');
-  const registered = await chrome.scripting.getRegisteredContentScripts({
-    ids: [CONTENT_SCRIPT_ID],
-  });
-  if (!!registered.length) {
-    console.log('Hook already installed');
-  } else {
-    console.log('Installing hook...');
-    chrome.scripting.registerContentScripts([{
-        id: CONTENT_SCRIPT_ID,
-        matches: ['<all_urls>'],
-        js: ['install_hook.js'],
-        runAt: 'document_start',
-        world: chrome.scripting.ExecutionWorld.MAIN,
-      },
-    ]);
-  }
+  port = chrome.runtime.connect({ name: 'knockknock' });
 };
 
 initialize();
