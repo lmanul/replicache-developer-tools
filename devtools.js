a94db49c2d39e8e53f831e51442e9ed1de818fad
Manu Cornet <m@ma.nu>
2022-12-07 02:01:03 +0000

    Add some more boilerplate/placeholders to start building a minimal UI in the devtools panel, and to have some content scripting

--- devtools.js
+++ devtools.js
@@ -1,2 +1,24 @@
 chrome.devtools.panels.create('Replicache', null,
     'panel.html', null);
+
+const getCurrentTab = async () => {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+};
+
+const initialize = async () => {
+  const currentTab = await getCurrentTab();
+  if (!currentTab) {
+    return;
+  }
+  chrome.scripting.executeScript({
+    target: { tabId: currentTab.id },
+    args: [],
+    func: () => {
+      console.log('Loading replicache dev tools content script');
+    },
+  });
+};
+
+initialize();
