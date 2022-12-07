52bac4c4b2b0d833d361781f48d96de10caa41d8
Manu Cornet <m@ma.nu>
2022-12-07 21:22:49 +0000

    Experiment with sending messages between the panel and the background script

--- panel.js
+++ panel.js
@@ -2,9 +2,20 @@ console.log('panel JS');
 
 let port;
 
-const initialize = async () => {
+const initializePanel = async () => {
   console.log('Initializing...');
+  const testButton = document.getElementById('test-message');
+  if (!!testButton) {
+    testButton.addEventListener('click', testMessage);
+  }
   port = chrome.runtime.connect({ name: 'knockknock' });
 };
 
-initialize();
+const testMessage = (evt) => {
+  port.postMessage({
+    'event': 'test',
+    'data': {'hello': 'world'},
+  });
+};
+
+initializePanel();
